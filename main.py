b460247e4ec48bca6b6512364022c0325745a956
Mileskay23 <820293@mystma.com>
2021-10-01 09:13:11 -0500

    push 1

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x,y):
+    if x > y:
+         return True
+    else:
+         return False
+
+print(greaterThan(5,2))
+
+def lessThan(x,y):
+    if x < y:
+        return True
+    else:
+        return False
+
+print(lessThan(4,5))
+
+def equalTo(x,y):
+    if x == y:
+        return True
+    else:
+        return False
+
+print(equalTo(8,8))
+
+def greaterOrEqual(x,y):
+    if x >= y:
+        return True
+    else:
+        return False
+
+print(greaterOrEqual(10,9))
+
+def lessOrEqual(x,y):
+    if x <= y:
+        return True
+    else:
+        return False
+
+    print(lessOrEqual(2,6))
